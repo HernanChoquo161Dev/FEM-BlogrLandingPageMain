d60c7ac591f4a2c18e9d87ab9972d5fe1a2d63e2
Hernan <hernanchoquedev@gmail.com>
2021-10-19 18:24:52 -0300

    add solution mobile design - CSS - HTML

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const menuOptions = document.querySelectorAll(".dropdown-menu__text");
 const dropdown = document.querySelectorAll(".dropdown");
 
+const imageMenu = document.querySelector(".nav-page__image-menu");
+const menu = document.querySelector(".nav-page__box-menu");
+
 var optionSelect = -1;
 
 function addEventClickToMenu(menuOptions, dropdown, index){
@@ -12,6 +15,10 @@ function addEventClickToMenu(menuOptions, dropdown, index){
         optionSelect = index;
     });
 }
+
+imageMenu.addEventListener('click', function (event) {
+    menu.classList.toggle("nav-page__box-menu--show");
+});
 
 for(a = 0; a < menuOptions.length; a++){
     addEventClickToMenu(menuOptions, dropdown, a);
