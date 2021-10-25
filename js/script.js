6f82efdcfd472666dfabc02ebb1205854f1cb974
Hernan <hernanchoquedev@gmail.com>
2021-10-25 09:06:07 -0300

    add animation menu option - CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const menuOptions = document.querySelectorAll(".dropdown-menu__text");
+const imageOptions = document.querySelectorAll(".dropdown-menu__image");
 const dropdown = document.querySelectorAll(".dropdown");
 
-const imageMenu = document.querySelector(".nav-page__image-menu");
+const showMenu = document.querySelector(".nav-page__image-menu");
 const menu = document.querySelector(".nav-page__box-menu");
 
 var optionSelect = -1;
@@ -10,10 +11,12 @@ function addEventClickToMenu(menuOptions, dropdown, index){
     menuOptions[index].addEventListener('click', function (event) {
 
         dropdown[index].classList.toggle("dropdown--show");
+        imageOptions[index].classList.toggle("dropdown-menu__image--transition");
         
         if(optionSelect != -1 && optionSelect != index){
             if(dropdown[optionSelect].classList.contains("dropdown--show")){
                 dropdown[optionSelect].classList.toggle("dropdown--show");
+                imageOptions[optionSelect].classList.toggle("dropdown-menu__image--transition");
             }
         }
 
@@ -22,7 +25,7 @@ function addEventClickToMenu(menuOptions, dropdown, index){
     });
 }
 
-imageMenu.addEventListener('click', function (event) {
+showMenu.addEventListener('click', function (event) {
     menu.classList.toggle("nav-page__box-menu--show");
 });
 
